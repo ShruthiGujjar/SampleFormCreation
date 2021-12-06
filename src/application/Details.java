34991e97e1a9888e4780d422beaeb036e105bfa6
ShruthiGujjar <95650937+ShruthiGujjar@users.noreply.github.com>
2021-12-07 00:59:42 +0530

    Committing the code changes

---
+++ src/application/Details.java
@@ -0,0 +1,33 @@
+package application;
+
+public class Details {
+
+	String tuserName,temailID;
+
+	public Details(String tuserName, String temailID) {
+	
+		this.tuserName = tuserName;
+		this.temailID = temailID;
+	}
+
+	public String getTuserName() {
+		return tuserName;
+	}
+
+	public void setTuserName(String tuserName) {
+		this.tuserName = tuserName;
+	}
+
+	public String getTemailID() {
+		return temailID;
+	}
+
+	public void setTemailID(String temailID) {
+		this.temailID = temailID;
+	}
+
+	
+	
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module DemoTableApplication {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+}
